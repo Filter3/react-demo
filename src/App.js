9c077fec6f3c029343a369182fb88a3eaddb44c6
Beatrice Gradanti <beatricegradanti@Air-di-Beatrice.lan>
2023-08-12 13:33:46 +0200

    first draft of app file

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Welcome to React App </h1>
+     <hr>
+     </hr>
+     <h3> August 12th 2023 </h3>
+     <hr></hr>
+     <hr></hr>
+    </div>
+  );
+}
+
+export default App;
